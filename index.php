eab7781dfcd5530a99c21b9e31d0aee9d103b395
SwixGaming <82118300+SwixGaming@users.noreply.github.com>
2021-10-18 10:22:13 -0400

    Update index.php

--- index.php
+++ index.php
@@ -100,7 +100,7 @@ function loginForm(){
                     });
                 }
  
-                setInterval (loadLog, 2500);
+                setInterval (loadLog, 0);
  
                 $("#exit").click(function () {
                     var exit = confirm("Are you sure you want to end the session?");
